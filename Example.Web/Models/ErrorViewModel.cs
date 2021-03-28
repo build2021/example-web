544817ab30cd6630a2054cebeaacb9bc95feb6bc
build2021 <jenkins.machi.pon@gmail.com>
2021-03-28 19:56:14 +0900

    Update settings

--- Example.Web/Models/ErrorViewModel.cs
+++ Example.Web/Models/ErrorViewModel.cs
@@ -1,9 +1,10 @@
-using System;
-
 namespace Example.Web.Models
 {
+    using System.Diagnostics.CodeAnalysis;
+
     public class ErrorViewModel
     {
+        [AllowNull]
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
